a9276ca529fb388ec5004e02e79d5b0a0ac99858
Tejashree Choramale <tejashreechoramale28@gmail.com>
2025-06-28 21:29:14 +0530

    Add localStorage to save and clear calculation

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,10 +1,17 @@
-let calculation = '';
+let calculation = localStorage.getItem('calculation') || '';
 
 function updateCalculation(n) {
   calculation += n;
   console.log(calculation);
+  localStorage.setItem('calculation', calculation);
 }
 
 function printCalculationString() {
   document.querySelector('.js-calculation-string').innerHTML = calculation;
+}
+
+function removeCalculationString() {
+  calculation = '';
+  localStorage.removeItem('calculation');
+  printCalculationString();
 }
